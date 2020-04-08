5bc63061b77f6cddc129cefb2ed4d23f50b5b27e
leventev <levente.vamos@protonmail.com>
2020-04-08 10:11:47 +0200

    Logger now flushes immediately after logging

--- src/logger.nim
+++ src/logger.nim
@@ -4,14 +4,18 @@ var
   consoleLogger = newConsoleLogger(fmtStr="[Server at $time][$levelname]: ")
   fileLogger = newFileLogger("log.txt", fmtStr="[Server at $time][$levelname]: ")
 
-proc info*(str: string) =
+proc logToConsole(lvl: Level, str: string) =
   consoleLogger.log(lvlInfo, str)
+  stdout.flushFile()
+
+proc info*(str: string) =
+  logToConsole(lvlInfo, str)
   fileLogger.log(lvlInfo, str)
 
 proc warn*(str: string) =
-  consoleLogger.log(lvlWarn, str)
+  logToConsole(lvlWarn, str)
   fileLogger.log(lvlWarn, str)
 
 proc error*(str: string) =
-  consoleLogger.log(lvlError, str)
+  logToConsole(lvlError, str)
   fileLogger.log(lvlError, str)
